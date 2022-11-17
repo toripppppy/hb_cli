fbd6c6c477e2ef9e378217c3087f4ffdca901dd1
toripppppy <Lundy11@icloud.com>
2022-11-17 13:11:12 +0900

    remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
-with open('requirements.txt') as requirements_file:
-    install_requirements = requirements_file.read().splitlines()
 
 setup(
     name='hbcli',
     version='1.0',
     description='Play Hit&Blow on your terminal!',
     packages=find_packages(),
-    install_requires=install_requirements,
     entry_points={
         'console_scripts':
             'hb = hb.core:main'
